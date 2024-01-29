b04cf4e134d2566f77c98c95e45508844cf304f2
kali <20001044+kali-muon@users.noreply.github.com>
2024-01-29 11:43:41 -0600

    working line state detection logic
    Co-authored-by: Asher S. <alienboy13@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,12 @@ enum LineCondition {
     Collision,
 }
 
+const BIT_PERIOD: u64 = 1000;
+const BIT_PERIOD_WITH_ERROR: u64 = 1130;
+const HALF_BIT_PERIOD: u64 = 500;
+const HALF_BIT_PERIOD_WITH_ERROR: u64 = 520;
+
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
@@ -44,39 +50,54 @@ async fn main(spawner: Spawner) {
     let mut line_state = LineCondition::Idle;
     let mut last_line_level = rx_pin.get_level();
 
+    info!("Waiting to enter loop...");
+    rx_pin.wait_for_any_edge().await;
+        println!("entering loop!");
     loop {
-        button.wait_for_rising_edge().await;
-        let new_line_level = rx_pin.get_level();
-        match line_state {
-            LineCondition::Idle => {
-                idle_led.set_high();
-                busy_led.set_low();
-                collision_led.set_low();
-
-                line_state = match new_line_level {
-                    Level::Low => LineCondition::Busy,
-                    Level::High => LineCondition::Idle,
+        match rx_pin.get_level() {
+            Level::High=> {
+                match select(Timer::after_micros(BIT_PERIOD_WITH_ERROR), rx_pin.wait_for_falling_edge()).await {
+                    Either::First(_) => {
+                        // line has gone idle
+                        println!("IDLE");
+                        line_state = LineCondition::Idle;
+                        idle_led.set_high();
+                        busy_led.set_low();
+                        collision_led.set_low();
+                        // wait for the line to fall before continuing
+                        rx_pin.wait_for_falling_edge().await;
+                    },
+                    Either::Second(_) => {
+                        // got a falling edge, so we're busy again
+                        continue;
+                        // line_state = LineCondition::Busy; // theoretically, i could replace this with a continue, since the next loop sets this to busy
+                    }
                 }
             },
-            LineCondition::Busy => {
+            Level::Low => {
+                println!("BUSY");
+                line_state = LineCondition::Busy;
                 idle_led.set_low();
                 busy_led.set_high();
-
-                line_state = match last_line_level {
-                    Level::Low => {
-                        match new_line_level {
-                            Level::Low => LineCondition::Collision,
-                            Level::High => LineCondition::Busy,
-                        }
+                collision_led.set_low();
+                match select(Timer::after_micros(BIT_PERIOD_WITH_ERROR), rx_pin.wait_for_rising_edge()).await {
+                    Either::First(_) => {
+                        // the collision timeout triggered
+                        println!("COLLISION");
+                        line_state = LineCondition::Collision;
+                        idle_led.set_low();
+                        busy_led.set_low();
+                        collision_led.set_high();
+                        // wait for the line to exit the collision state
+                        rx_pin.wait_for_rising_edge().await; // this will need a refactor to support the random backoffs later
                     },
-                    Level::High => {},
-            },
-            LineCondition::Collision => {
-                line_state = LineCondition::Idle;
-                idle_led.set_low();
-                busy_led.set_low();
-                collision_led.set_high();
-            },
+                    Either::Second(_) => {
+                        // keep line marked as busy
+                        continue;
+                        // line_state = LineCondition::Busy;
+                    }
+                }
+            }
         }
     }
 }
