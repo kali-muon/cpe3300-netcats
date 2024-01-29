418b6523b60f2ff47a96a6381a145dd5af22c636
kali <20001044+kali-muon@users.noreply.github.com>
2024-01-28 22:56:43 -0600

    basic blinky code working

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::{exti::ExtiInput, gpio::{Input, Level, Output, Pull, Speed}};
-use embassy_time::Timer;
+use embassy_time::{Timer};
 use embassy_futures::select::select;
 use embassy_futures::select::Either;
 use {defmt_rtt as _, panic_probe as _};
@@ -20,14 +20,20 @@ enum LineCondition {
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
-    let mut idle_led = Output::new(p.PB5, Level::Low, Speed::Low);
-    let mut busy_led = Output::new(p.PB6, Level::Low, Speed::Low);
-    let mut collision_led = Output::new(p.PB7, Level::Low, Speed::Low);
+    let mut idle_led = Output::new(p.PB15, Level::Low, Speed::Low);
+    let mut busy_led = Output::new(p.PB14, Level::Low, Speed::Low);
+    let mut collision_led = Output::new(p.PB13, Level::Low, Speed::Low);
 
-    let rx_thing = Input::new(p.PC8, Pull::None);
-    let mut rx_pin = ExtiInput::new(rx_thing, p.EXTI8);
+    let rx_pin = Input::new(p.PC8, Pull::None);
+    let mut rx_pin = ExtiInput::new(rx_pin, p.EXTI8);
 
     let mut line_state = LineCondition::Idle;
 
-    loop {}
+    let mut onboard_led = Output::new(p.PA5, Level::Low, Speed::Low);
+
+    loop {
+        info!("toggling! {}", embassy_time::TICK_HZ);
+        collision_led.toggle();
+        Timer::after_millis(100).await;
+    }
 }
