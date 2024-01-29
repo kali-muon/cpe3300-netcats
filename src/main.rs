bcf6bb165276a155ad5486d2d99e530884c1af3a
kali <20001044+kali-muon@users.noreply.github.com>
2024-01-29 00:11:59 -0600

    basic state machine demo

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,19 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{exti::ExtiInput, gpio::{Input, Level, Output, Pull, Speed}};
-use embassy_time::{Timer};
 use embassy_futures::select::select;
 use embassy_futures::select::Either;
+use embassy_stm32::exti::AnyChannel;
+use embassy_stm32::exti::Channel;
+use embassy_stm32::gpio::AnyPin;
+use embassy_stm32::gpio::Pin;
+use embassy_stm32::{
+    exti::ExtiInput,
+    gpio::{Input, Level, Output, Pull, Speed},
+};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 enum LineCondition {
@@ -16,8 +25,10 @@ enum LineCondition {
     Collision,
 }
 
+static LINE_STATE: Mutex<ThreadModeRawMutex, LineCondition> = Mutex::new(LineCondition::Idle);
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     let mut idle_led = Output::new(p.PB15, Level::Low, Speed::Low);
@@ -27,13 +38,57 @@ async fn main(_spawner: Spawner) {
     let rx_pin = Input::new(p.PC8, Pull::None);
     let mut rx_pin = ExtiInput::new(rx_pin, p.EXTI8);
 
-    let mut line_state = LineCondition::Idle;
-
     let mut onboard_led = Output::new(p.PA5, Level::Low, Speed::Low);
 
+    info!("about to spawn");
+    spawner.spawn(state_logic(p.PC13.degrade(), p.EXTI13.degrade())).unwrap();
+    info!("spawned!");
+
     loop {
-        info!("toggling! {}", embassy_time::TICK_HZ);
-        collision_led.toggle();
-        Timer::after_millis(100).await;
+        let state = LINE_STATE.lock().await; 
+        match *state {
+            LineCondition::Idle => {
+                idle_led.set_high();
+                busy_led.set_low();
+                collision_led.set_low();
+            },
+            LineCondition::Busy => {
+                idle_led.set_low();
+                busy_led.set_high();
+                collision_led.set_low();
+            },
+            LineCondition::Collision => {
+                idle_led.set_low();
+                busy_led.set_low();
+                collision_led.set_high();
+            },
+        }
+        drop(state);
+        Timer::after_micros(500).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn state_logic(mut pin: AnyPin, exti: AnyChannel) {
+    let button = Input::new(pin, Pull::None);
+    let mut button = ExtiInput::new(button, exti);
+
+    info!("entering button loop");
+    loop {
+        button.wait_for_falling_edge().await;
+        info!("button pressed??????");
+        let mut state = LINE_STATE.lock().await;
+        match *state {
+            LineCondition::Idle => {
+                *state = LineCondition::Busy;
+            },
+            LineCondition::Busy => {
+                *state = LineCondition::Collision;
+            },
+            LineCondition::Collision => {
+                *state = LineCondition::Idle;
+            },
+        }
+        drop(state);
     }
 }
